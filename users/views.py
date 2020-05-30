d06d27fc4d1264eac4743f079f48f2e09bee7de4
Ndonye Mark <ndonyemark@gmail.com>
2020-05-30 12:07:28 +0300

    add the form.py registration class and views.py register view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistration
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def register(request):
-
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistration()
+        if form.is_valid():
+            form.save()
+            messages.success("Account has been created! You can now login")
+            return redirect('login')
+    else:
+        form = UserRegistration()
+    title = 'User Registration'
+    return render(request, 'users/register.html', {'form': form, 'title': title})

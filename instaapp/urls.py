34c943124f608a952bf306db6f6646c0ea1337b3
mark-ndonye <gitaundonye4@gmail.com>
2020-05-30 23:51:57 +0300

    add single image url path

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
-    path('post_image', views.post_image, name='post_image')
+    path('post_image', views.post_image, name='post_image'),
+    path('single_image', views.single_image_details, name='single_image'),
 ]

39e3b56e669e4c49d7e2c89a0f3a761560a1a18d
mark-ndonye <gitaundonye4@gmail.com>
2020-05-30 15:02:37 +0300

    add comments, image and profile models

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField()
+    profile_bio = models.TextField()
+
+    def save_profile(self):
+        return self.save()
+    
+    def delete_profile(self):
+        return self.delete()
+
+class Image(models.Model):
+    image = models.ImageField()
+    image_name = models.CharField(max_length = 40)
+    image_caption = models.CharField(max_length = 20)
+    image_comments = models.ManyToManyField(related_name='comment')
+    image_profile_foreign_key = models.ForeignKey(on_delete=models.CASCADE)
+
+    def save_image(self):
+        return self.save()
+    
+    def delete_image(self):
+        return self.delete()
+    
+    @classmethod
+    def get_single_image_details(cls, image_id):
+        single_image_details = Image.objects.get(id = image_id)
+        return single_image_details
+    
+    @classmethod
+    def get_all_images(cls):
+        all_images = Image.objects.all()
+        return all_images
+
+class Comments(models.Model):
+    comment = models.TextField()

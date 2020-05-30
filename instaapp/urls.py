263a8cb122e379fb5f05fd80535e31efd2fcfd91
mark-ndonye <gitaundonye4@gmail.com>
2020-05-31 00:04:25 +0300

    added single image data

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('post_image', views.post_image, name='post_image'),
-    path('single_image', views.single_image_details, name='single_image'),
+    path('index/', views.index, name='index'),
+    path('post_image/', views.post_image, name='post_image'),
+    path('single_image/<int:image_id>/', views.single_image_details, name='single_image'),
 ]

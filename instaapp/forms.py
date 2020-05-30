b645cae826dcf483060624245a20d1e2f941c82a
mark-ndonye <gitaundonye4@gmail.com>
2020-05-30 20:48:03 +0300

    add order of all image fields

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -5,4 +5,5 @@ from .models import Image
 class ImageRegistrationForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['pub_date', 'posted_by']
+        exclude = ['pub_date', 'posted_by', 'image_profile_foreign_key']
+        fields = ['image_caption', 'image_name', 'image']

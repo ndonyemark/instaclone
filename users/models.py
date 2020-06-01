964f3ffc65a5832ec957d9441d5644c39d02a1bb
mark-ndonye <gitaundonye4@gmail.com>
2020-06-01 10:59:16 +0300

    created signals.py and updated the models

--- users/models.py
+++ users/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_bio = models.TextField()
     profile_pic = models.ImageField()
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self .image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)

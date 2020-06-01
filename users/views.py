f508d85d59fc964d7e4e823ae2a10b20b3a63601
mark-ndonye <gitaundonye4@gmail.com>
2020-06-01 11:51:06 +0300

    add update info

--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistration
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
@@ -16,6 +19,20 @@ def register(request):
     return render(request, 'users/register.html', {'form': form, 'title': title})
 
 def profile(request):
-
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.Post, instance=request.user)
+        p_form = ProfileUpdateForm(request.Post, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success('You have successfully updated your profile!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
     title = 'Profile'
-    return render(request, 'users/profile.html', {'title': title})
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', {'title': title}, context)
